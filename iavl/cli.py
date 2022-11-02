5b0166a475e45abe06fe3655c66c026931b81654
HuangYi <huang@crypto.com>
2022-11-02 15:56:06 +0800

    node command

--- iavl/cli.py
+++ iavl/cli.py
@@ -2,11 +2,17 @@ import binascii
 from typing import List, Optional
 
 import click
+import cprotobuf
 import rocksdb
+from hexbytes import HexBytes
 
 
 def root_key(v: int) -> bytes:
     return b"r" + v.to_bytes(8, "big")
+
+
+def node_key(hash: bytes) -> bytes:
+    return b"n" + hash
 
 
 def store_prefix(s: str) -> bytes:
@@ -28,6 +34,12 @@ def prev_version(db: rocksdb.DB, store: str, v: int) -> int:
 
 def latest_version(db: rocksdb.DB, store: str) -> int:
     return prev_version(db, store, 1 << 63 - 1)
+
+
+def decode_bytes(bz: bytes) -> (bytes, int):
+    l, n = cprotobuf.decode_primitive(bz, "uint64")
+    assert l + n <= len(bz)
+    return bz[n : n + l], n + l
 
 
 @click.group
@@ -55,6 +67,45 @@ def root_hash(db, store: List[str], version: Optional[int]):
             version = latest_version(db, s)
         bz = db.get(store_prefix(s) + root_key(version))
         print(f"{s}: {binascii.hexlify(bz).decode()}")
+
+
+@cli.command()
+@click.option("--db", help="path to application.db", type=click.Path(exists=True))
+@click.option("--store", "-s")
+@click.argument("hash")
+def node(db, hash, store):
+    db = rocksdb.DB(str(db), rocksdb.Options(), read_only=True)
+    bz = db.get(store_prefix(store) + node_key(HexBytes(hash)))
+    offset = 0
+    height, n = cprotobuf.decode_primitive(bz[offset:], "int64")
+    offset += n
+    size, n = cprotobuf.decode_primitive(bz[offset:], "int64")
+    offset += n
+    version, n = cprotobuf.decode_primitive(bz[offset:], "int64")
+    offset += n
+    key, n = decode_bytes(bz[offset:])
+    offset += n
+
+    print(
+        f"height: {height}, size: {size}, version: {version}, "
+        f"key: {HexBytes(key).hex()}, ",
+        end="",
+    )
+    if height == 0:
+        # leaf node, read value
+        value, n = decode_bytes(bz[offset:])
+        offset += n
+        print(f"value: {HexBytes(value).hex()}")
+    else:
+        # container node, read children
+        left_hash, n = decode_bytes(bz[offset:])
+        offset += n
+        right_hash, n = decode_bytes(bz[offset:])
+        offset += n
+        print(
+            f"left hash: {HexBytes(left_hash).hex()}, "
+            f"right hash: {HexBytes(right_hash).hex()}"
+        )
 
 
 @cli.command()
